db57bf58094d61afcb95d5f10e1c2844c84cdcf8
Daniel Belokon <daniel.belokon@gmail.com>
2021-12-31 19:03:00 +0200

    add manager.h with pdf-specitied funcs

--- solution/workplace.h
+++ solution/workplace.h
@@ -1,4 +1,41 @@
-#ifndef WORKPLACE_H_
-#define WORKPLACE_H_
+#ifndef WORKPLACE_H
+#define WORKPLACE_H
 
-#endif // WORKPLACE_H_
+#include <string>
+#include <map>
+
+#include "employee.h"
+#include "manager.h"
+
+namespace mtm
+{
+    class Workplace
+    {
+    private:
+        long id;
+        long worker_salary;
+        long manager_salary;
+        std::string name;
+
+        std::map<long, Manager *> managers;
+
+    public:
+        Workplace(long id, std::string workplaceName, long workerSalary, long managerSalary);
+
+        long getId();
+        std::string getName();
+        long getWorkerSalary();
+        long getManagerSalary();
+
+        template <class Condition>
+        void hireEmployee(Condition isHirable, Employee *employee, int managerId);
+        void fireEmployee(long employeeId, long managerId);
+
+        void hireManager(Manager *manager);
+        void fireManager(long managerId);
+
+        friend std::ostream &operator<<(std::ostream &os, const Workplace &workplace);
+    };
+}
+
+#endif // WORKPLACE_H
