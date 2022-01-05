e846a9fcad9e06942939fde16bb5d18f227602a0
Daniel Belokon <daniel.belokon@gmail.com>
2022-01-05 22:12:46 +0200

    add empty city funcs

---
+++ solution/city.cpp
@@ -0,0 +1,58 @@
+#include <string>
+
+#include "city.h"
+
+mtm::City::City(std::string name) : name(name)
+{
+}
+
+void mtm::City::addFaculty()
+{
+    throw std::string("NotImplemented");
+}
+
+void mtm::City::createWorkplace()
+{
+    throw std::string("NotImplemented");
+}
+
+void mtm::City::teachAtFaculty(long workerId, long facultyId)
+{
+    throw std::string("NotImplemented");
+}
+
+template <class Condition>
+void mtm::City::hireEmployeeAtWorkplace(Condition isHireable, long employeeId, long managerId, long workplaceId)
+{
+    throw std::string("NotImplemented");
+}
+
+void mtm::City::hireManagerAtWorkplace(long managerId, long workplaceId)
+{
+    throw std::string("NotImplemented");
+}
+
+void mtm::City::fireEmployeeAtWorkplace(long employeeId, long workplaceId)
+{
+    throw std::string("NotImplemented");
+}
+
+void mtm::City::fireManagerAtWorkplace(long managerId, long workplaceId)
+{
+    throw std::string("NotImplemented");
+}
+
+void mtm::City::getAllAboveSalary(std::ostream &os, long salary)
+{
+    throw std::string("NotImplemented");
+}
+
+bool mtm::City::isWorkingInTheSameWorkplace(long employeeOneId, long employeeTwoId)
+{
+    throw std::string("NotImplemented");
+}
+
+void mtm::City::prointAllEmployeesWithSkill(std::ostream &os, Skill skill)
+{
+    throw std::string("NotImplemented");
+}
