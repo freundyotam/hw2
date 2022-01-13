a56d1c890b52e6e69769e3daff31b5edbe687a2e
yotam freund <freund.yotam@gmail.com>
2022-01-13 16:48:46 +0200

    Tests for manager

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,20 +5,22 @@
 #include "employee_print_test.h"
 #include "manager_print_test.h"
 #include "workplace_print_test.h"
+#include "workplace_test.h"
 
 int main(int argc, char** argv)
 {
-    main_city_test();
-    main_employee_print_test();
-    check_skill_members();
-    check_skill_logic_operators();
-    check_skill_printing();
-    check_employee_members();
-    check_employees_skill_functionality();
-    check_employee_printing();
-    test_employee_clone();
-    manager_print_test();
-    workplace_test();
-    test_manager();
+//    main_city_test();
+//    main_employee_print_test();
+//    check_skill_members();
+//    check_skill_logic_operators();
+//    check_skill_printing();
+//    check_employee_members();
+//    check_employees_skill_functionality();
+//    check_employee_printing();
+//    test_employee_clone();
+//    manager_print_test();
+//    workplace_test();
+//    test_manager();
+    test_workplace();
 }
 
--- tests/workplace_test.h
+++ tests/workplace_test.h
@@ -4,5 +4,6 @@
 
 #ifndef UNTITLED_WORKPLACE_TEST_H
 #define UNTITLED_WORKPLACE_TEST_H
-
+#include "employee_test.h"
+void test_workplace();
 #endif //UNTITLED_WORKPLACE_TEST_H
